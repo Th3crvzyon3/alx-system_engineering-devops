7bd56c4380a7281f5ba42cf134f2d7bc22a3b5d8
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-11-03 01:04:09 +0300

    initial commit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""A recursive function that queries the Reddit API, parses the title of all
-hot articles, and prints a sorted count of given keywords"""
-
+""" Recursive function that queries the Reddit API
+    parses the title of all hot articles, and prints a sorted count"""
 import requests
+import sys
+after = None
+count_dic = []
 
 
 def count_words(subreddit, word_list):
-    """parses the title of all hot articles and prints a sorted count"""
-    r = requests.get(r'https://www.reddit.com/r/{}/hot/.json'
-                     .format(subreddit), headers={'User-agent': 'x'},
-                     allow_redirects=False)
-    l = r.json().get('data').get('children')
-    if r.status_code != 200:
-        return None
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
